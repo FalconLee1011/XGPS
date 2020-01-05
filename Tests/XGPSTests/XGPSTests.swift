545bec81ade4f88e69ba27dfca39a5d45c0c47a1
FalconXCLi <falcon.lee1011@gmail.com>
2020-01-06 05:42:02 +0800

    Commit

--- Tests/XGPSTests/XGPSTests.swift
+++ Tests/XGPSTests/XGPSTests.swift
@@ -6,7 +6,7 @@ final class XGPSTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(XGPS.XGPSlib)
+        XCTAssertEqual(XGPS().text, "Hello, World!")
     }
 
     static var allTests = [
